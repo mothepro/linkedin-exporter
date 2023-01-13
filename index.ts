9687b2e8cac06abf9eac3a0f22a18fdd36687077
Mo Prosper <mothepro@live.com>
2023-01-13 16:04:26 -0600

    format date

--- index.ts
+++ index.ts
@@ -133,7 +133,7 @@ try {
   }
 
   assert(data.size, 'No data was found to export')
-  download(`exported_${time}.csv`, toCsv(data))
+  download(`Contacts Exported on ${time}.csv`, toCsv(data))
 } catch (err) {
   assert(err instanceof Error)
   alert(err.message)
